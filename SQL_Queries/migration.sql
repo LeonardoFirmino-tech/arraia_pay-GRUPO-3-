9c4c888b3b8ed955863c5470188572e856936834
Danilo <danikehiga@gmail.com>
2026-09-21 08:19:00 -0300

    Adicionado tabelas de barraca e produto

--- SQL_Queries/migration.sql
+++ SQL_Queries/migration.sql
@@ -25,3 +25,33 @@ CREATE TABLE `cliente` (
   KEY `fk_cliente_cartao` (`id_cartao`),
   CONSTRAINT `fk_cliente_cartao` FOREIGN KEY (`id_cartao`) REFERENCES `cartao` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+ -- arraiapay.barraca definição
+ 
+CREATE TABLE barraca (
+id INT AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL,
+responsavel VARCHAR(100) NOT NULL,
+ativa BOOLEAN NOT NULL,
+PRIMARY KEY (id)
+);
+
+
+
+ -- arraiapay.produto definição
+
+CREATE TABLE produto (
+id INT AUTO_INCREMENT,
+barraca_id INT NOT NULL,
+nome VARCHAR(100) NOT NULL,
+categoria VARCHAR(50) NOT NULL,
+preco DECIMAL (5, 2) NOT NULL,
+estoque INT,
+estoque_minimo INT,
+ativo BOOLEAN NOT NULL,
+PRIMARY KEY (id),
+
+FOREIGN KEY (barraca_id) REFERENCES barraca(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
